ae9ad3712e34a2a4e3909620885a8f573f9caaf2
rea-jin <56153015+rea-jin@users.noreply.github.com>
2025-01-01 16:05:50 +0900

    complete

--- pages/profile/[userId].tsx
+++ pages/profile/[userId].tsx
@@ -1,10 +1,11 @@
 import apiClient from "@/src/lib/apiClient";
 import React from "react";
 import { GetServerSideProps } from "next";
-import { Profile } from "@/src/types";
+import { PostType, Profile } from "@/src/types";
 
 type Props = {
   profile: Profile;
+  posts: PostType[];
 };
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
@@ -12,9 +13,11 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
 
   try {
     const profileResponse = await apiClient.get(`/users/profile/${userId}`);
+    const postsResponse = await apiClient.get(`/posts/${userId}`);
     return {
       props: {
         profile: profileResponse.data,
+        posts: postsResponse.data,
       },
     };
   } catch (error) {
@@ -24,7 +27,8 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
     };
   }
 };
-const UserProfile = ({ profile }: Props) => {
+
+const UserProfile = ({ profile, posts }: Props) => {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="w-full max-w-xl mx-auto">
@@ -43,18 +47,27 @@ const UserProfile = ({ profile }: Props) => {
             </div>
           </div>
         </div>
-        <div className="bg-white shadow-md rounded p-4 mb-4">
-          <div className="mb-4">
-            <div className="flex items-center mb-2">
-              <img className="w-10 h-10 rounded-full mr-2" alt="User Avatar" />
-              <div>
-                <h2 className="font-semibold text-md">shincode</h2>
-                <p className="text-gray-500 text-sm">2023/05/08</p>
+        {/* postsは配列 */}
+        {posts.map((post: PostType) => (
+          <div className="bg-white shadow-md rounded p-4 mb-4" key={post.id}>
+            <div className="mb-4">
+              <div className="flex items-center mb-2">
+                <img
+                  className="w-10 h-10 rounded-full mr-2"
+                  alt="User Avatar"
+                  src={profile.profileImageUrl}
+                />
+                <div>
+                  <h2 className="font-semibold text-md">
+                    {post.author.username}
+                  </h2>
+                  <p className="text-gray-500 text-sm">{post.createdAt}</p>
+                </div>
               </div>
+              <p className="text-gray-700">{post.content}</p>
             </div>
-            <p className="text-gray-700">はじめての投稿です。</p>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { PostType } from '../types';
-import Image from 'next/image';
+import React from "react";
+import { PostType } from "../types";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
-  post : PostType;
+  post: PostType;
+};
 
-}
-
-const Post = (props : Props) => {
+const Post = (props: Props) => {
   const { post } = props;
 
   console.log(post);
@@ -15,26 +15,29 @@ const Post = (props : Props) => {
 
   return (
     <div className="bg-white shadow-md rounded p-4 mb-4">
-    <div className="mb-4">
+      <div className="mb-4">
         <div className="flex items-center mb-2">
-        <Image
-            className="w-10 h-10 rounded-full mr-2"
-            src={post.author.profile?.profileImageUrl}
-            alt="User Avatar"
-            width={40}
-            height={40}
-        />
-        <div>
-          {/* ?をつけることで trueの時のみ表示？ */}
-            <h2 className="font-semibold text-md">{ post.author?.username}</h2>
+          <Link href={`/profile/${post.author.id}`}>
+            <Image
+              className="w-10 h-10 rounded-full mr-2"
+              src={post.author.profile?.profileImageUrl}
+              alt="User Avatar"
+              width={40}
+              height={40}
+            />
+          </Link>
+          <div>
+            {/* ?をつけることで trueの時のみ表示？ */}
+            <h2 className="font-semibold text-md">{post.author?.username}</h2>
             <p className="text-gray-500 text-sm">
-              { new Date(post.createdAt).toLocaleString() }</p>
+              {new Date(post.createdAt).toLocaleString()}
+            </p>
+          </div>
         </div>
-        </div>
-        <p className="text-gray-700">{ post.content }</p>
+        <p className="text-gray-700">{post.content}</p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
