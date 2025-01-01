8157d6a3dc700e3d419cc9ec6f7d089874d4e3ff
rea-jin <56153015+rea-jin@users.noreply.github.com>
2025-01-01 19:06:53 +0900

    base url

--- src/lib/apiClient.tsx
+++ src/lib/apiClient.tsx
@@ -3,10 +3,11 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:4000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  // baseURL: "http://localhost:4000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
